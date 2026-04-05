be1438edada93f4a7a16d06f1b566d1243386d11
GitExl <GitExl@users.noreply.github.com>
2026-04-05 10:49:42 +0200

    Clarify sublicense for renderlib code.

--- renderlib/src/presenter.c
+++ renderlib/src/presenter.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2016, Dennis Meuwissen
+  Copyright (c) 2026, Dennis Meuwissen
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
--- src/renderlib/utils.py
+++ src/renderlib/utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016, Dennis Meuwissen
+# Copyright (c) 2026, Dennis Meuwissen
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
